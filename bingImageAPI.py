d62608e7de1696fbd5e9d255776932941c423fab
UltimatePea <superchenzb@gmail.com>
2017-03-31 12:05:20 -0700

    Finish functions

--- bingImageAPI.py
+++ bingImageAPI.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python
+import os
 import requests
+from subprocess import call
 
+os.system('rm -rf build')
+os.system('mkdir build')
+os.chdir('build')
 
 def writeToFile(r, path):
     with open(path, 'wb') as f:
@@ -30,9 +36,20 @@ def request(searchName, imageName):
             filename = imageName + "." + str(entry["encodingFormat"])
             print "successful, writing to " + filename
             writeToFile(imgr, filename)
-            break
-    else:
-        print "Image not found " + searchName
+            return filename
+    print "Image not found " + searchName
 
 
-request("iPhone", "image00003")
+def generateVideo(searchString, imageName):
+    filename = request(searchString, imageName)
+    os.system('echo "' +  searchString + '" > textToBeConverted')
+    os.system('say -f textToBeConverted -o converted.aiff')
+    video_name = filename + "_out.mp4"
+    os.system("echo file \\'" + video_name + "\\' >> myfileList.txt")
+    os.system("ffmpeg -i converted.aiff -i " + filename + " " + video_name)
+
+def concat(outputVideoName):
+    os.system("ffmpeg -f concat -i myfileList.txt -c copy " + outputVideoName + ".mp4")
+
+generateVideo("The United States of America (USA), commonly known as the United States (U.S.) or America, is a constitutional federal republic composed of 50 states, a federal district, five major self-governing territories, and various possessions.", "img")
+
