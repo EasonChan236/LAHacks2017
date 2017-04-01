33446fee8d5420a3b83c2bde9e7d90f52bc57e05
Ultimate Pea <superchenzb@gmail.com>
2017-04-01 11:52:47 -0700

    Add files via upload

--- bingImageAPI.py
+++ bingImageAPI.py
@@ -3,6 +3,7 @@ import os
 import re
 import requests
 from subprocess import call
+import cog_speech
 
 
 os.system('rm -rf build')
@@ -65,14 +66,24 @@ def generateVideo(searchString, imageName):
         print "Error in creating " + searchString
         return
     audio_name = imageName + "_audio.aiff"
-    os.system('echo "' +  searchString + '" > textToBeConverted')
-    os.system('say -f textToBeConverted -o ' + audio_name)
-    video_name = filename + "_out.mp4"
-    os.system("echo file \\'" + video_name + "\\' >> myfileList.txt")
-    os.system("ffmpeg -loop 1 -f image2 -i " + filename + " -i " + audio_name + " -shortest -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" " + video_name + debugFlag())
+    #os.system('echo "' +  searchString + '" > textToBeConverted')
+    #os.system('say -f textToBeConverted -o ' + audio_name)
+#create new audio name
+    audio_name = cog_speech.generateAudio(searchString, audio_name)
+    video_name = filename + "_out.m4v"
+    os.system("echo file '" + video_name + "' >> myfileList.txt")
+    os.system("ffmpeg -loop 1 -f image2 -i " + filename + " -i " + audio_name + " -shortest -f mpeg -pix_fmt yuv420p -vf \"scale=trunc(iw/2)*2:trunc(ih/2)*2\" " + video_name + debugFlag())
 
 
 def concat(outputVideoName):
+    #with open("myfileList.txt") as f:
+    #    command = "MP4Box -force-cat "
+    #    for line in f:
+    #        command += "-cat " + line.strip()
+    #    command += "-new " + outputVideoName + ".mp4"
+    #os.system(command)
+        
+
     os.system("ffmpeg -f concat -i myfileList.txt -pix_fmt yuv420p " + outputVideoName + ".mp4" + debugFlag())
 
 
@@ -84,11 +95,11 @@ def generateSentenceVideo(sentence, outputVideoName):
 
 #generateSentenceVideo("The United States of America (USA), commonly known as the United States (U.S.) or America, is a constitutional federal republic composed of 50 states, a federal district, five major self-governing territories, and various possessions.", "output")
 content = """
-iPhone  is a line of smartphones designed and marketed by Apple Inc. They run Apple's iOS mobile operating system.[15] The first generation iPhone was released on June 29, 2007; the most recent iPhone model is the iPhone 7, which was unveiled at a special event on September 7, 2016.[16][17]
-"""
+Microsoft Windows (or simply Windows) is a metafamily of graphical operating systems developed, marketed, and sold by Microsoft. It consists of several families of operating systems, each of which cater to a certain sector of the computing industry with the OS typically associated with IBM PC compatible architecture. Active Windows families include Windows NT, Windows Embedded and Windows Phone; these may encompass subfamilies, e.g. Windows Embedded Compact (Windows CE) or Windows Server. Defunct Windows families include Windows 9x; Windows 10 Mobile is an active product, unrelated to the defunct family Windows Mobile.
 
-para2="""
-The user interface is built around the device's multi-touch screen, including a virtual keyboard. The iPhone has Wi-Fi and can connect to cellular networks. An iPhone can shoot video (though this was not a standard feature until the iPhone 3GS), take photos, play music, send and receive email, browse the web, send and receive text messages, follow GPS navigation, record notes, perform mathematical calculations, and receive visual voicemail.[18] Other functionality, such as video games, reference works, and social networking, can be enabled by downloading mobile apps. As of January 2017, Apple's App Store contained more than 2.2 million applications available for the iPhone and other iOS devices.[19]
+Microsoft introduced an operating environment named Windows on November 20, 1985, as a graphical operating system shell for MS-DOS in response to the growing interest in graphical user interfaces (GUIs).[4] Microsoft Windows came to dominate the world's personal computer (PC) market with over 90% market share, overtaking Mac OS, which had been introduced in 1984. Apple came to see Windows as an unfair encroachment on their innovation in GUI development as implemented on products such as the Lisa and Macintosh (eventually settled in court in Microsoft's favor in 1993). On PCs, Windows is still the most popular operating system. However, in 2014, Microsoft admitted losing the majority of the overall operating system market to Android,[5] because of the massive growth in sales of Android smartphones. In 2014, the number of Windows devices sold was less than 25% that of Android devices sold. This comparison however may not be fully relevant, as the two operating systems traditionally target different platforms.
+
+As of September 2016, the most recent version of Windows for PCs, tablets, smartphones and embedded devices is Windows 10. The most recent versions for server computers is Windows Server 2016. A specialized version of Windows runs on the Xbox One game console.[6]
 """
 generateSentenceVideo(content, "output");
 print "Output saved to build/output.mp4"
