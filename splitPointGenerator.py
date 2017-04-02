382f2f6119012fda9723683b74c40de89fb04344
hurenxu <reh011@ucsd.edu>
2017-04-01 19:09:56 -0700

    split point generator update

--- splitPointGenerator.py
+++ splitPointGenerator.py
@@ -17,20 +17,30 @@ def generateSplit( audioFileName, arrayOfSplitPoints ):
     # keep track of the precious time
     previousTime = videoTimeFirst
        
-    for index in range(len(arrayOfSplitPoints)):
+    for index,_ in enumerate(arrayOfSplitPoints):
       if index == 0:
         #os.system("ffmpeg -i '" + audioFileName + "'.mp3 -f segment -segment_time '" + str(videoTimeFirst) + "' -c copy '" + audioFileName + str(index) + "'.mp3")
 	os.system("ffmpeg -ss " + str(0) + " -t " + str(videoTimeFirst) + " -i " + audioFileName + ".mp3 out" + str(index) + ".mp3")
 	#outputFileName[index] = audioFileName + str(index) + ".mp3"
-	outputFileName[index] = "out" + str(index) + ".mp3"
+	outputFileName.append("out" + str(index) + ".mp3")
+      #elif index == (len(arrayOfSplitPoints) - 1):
+       # videoTime = track_length_in_milliseconds * arrayOfSplitPoints[index]
+        #tempTime = videoTime	
+	#prev = previousTime
+        #videoTime = videoTime - previousTime
+        #previousTime = tempTime	
+	#outputFileName.append("out" + str(index) + ".mp3")
+	#os.system("ffmpeg -ss " + str(prev) + " -i " + audioFileName + ".mp3 out" + str(index) + ".mp3")
       else: 
-        videoTime = track_length_in_milliseconds * arrayOfSplitPoints[index]
+	print("index " + str(index))
+	print("array " + str(arrayOfSplitPoints[index]))
+        videoTime = float(track_length_in_milliseconds) * arrayOfSplitPoints[index]
         tempTime = videoTime	
 	prev = previousTime
         videoTime = videoTime - previousTime
         previousTime = tempTime	
 	#outputFileName[index] = audioFileName + str(index) + ".mp3"
-	outputFileName[index] = "out" + str(index) + ".mp3"
+	outputFileName.append("out" + str(index) + ".mp3")
         #os.system("ffmpeg -i '" + audioFileName + str((index - 1)) + "'.mp3 -f segment -segment_time '" + str(videoTime) + "' -c copy '" + audioFileName + str(index) + "'.mp3")
 	os.system("ffmpeg -ss " + str(prev) + " -t " + str(videoTime) + " -i " + audioFileName + ".mp3 out" + str(index) + ".mp3")
     return outputFileName
